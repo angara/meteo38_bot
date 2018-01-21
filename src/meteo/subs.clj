590b6f31a1a755267f07e2f9d3c234f7f634ccfe
Maxim Penzin <maxp@ya.ru>
2018-01-22 07:42:32 +0800

    meteo: cmd-adds fix

--- src/meteo/subs.clj
+++ src/meteo/subs.clj
@@ -138,10 +138,6 @@
         favs (not-empty (get-favs cid))
         subs (not-empty (get-subs cid))]
     (cond
-      (not= cid (-> msg :from :id))
-      (tg/send-text apikey cid 
-        "Команда не рабтает в групповом чате.\nПерейдите в персональный чат с ботом: @meteo38bot")
-
       (not favs)
       (tg/send-text apikey cid "Ничего нет в Избранном!")
 
